120dc9b69d0a6a40a03ac26594fb9c209658cbfe
Claiyre_ <mayingying1996@163.com>
2018-02-22 22:28:37 +0800

    add toc-t

--- picidae.config.js
+++ picidae.config.js
@@ -13,7 +13,9 @@ module.exports = {
     themeConfigsRoot: './theme-configs',
     hotReloadTests: [/\.(md|markdown)$/i],
     excludes: [/\.ignore\./],
-    transformers: [],
+    transformers: [
+        'picidae-transformer-toc?maxDepth=8&test=null',
+    ],
     commanders: [
         'gh-pages?repo=git@github.com:Claiyre/Claiyre.github.io.git&branch=master&remote=origin'
     ]
